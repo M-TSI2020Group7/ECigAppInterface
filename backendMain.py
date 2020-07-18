beaf68cb092bafdacde79b27a28b3291663f7ece
Sujay Swain <susw12@noreply.github.com>
2020-07-18 15:06:35 -0400

    I added something :/

--- backendMain.py
+++ backendMain.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index ():
