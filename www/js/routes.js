828d4848fd590fbfedf1ca6cced0e4bd2feed4c9
Sujay Swain <susw12@users.noreply.github.com>
2020-07-20 21:55:30 -0400

    Add files via upload

---
+++ www/js/routes.js
@@ -0,0 +1,57 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('menu.progress', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/progress.html',
+        controller: 'progressCtrl'
+      }
+    }
+  })
+
+  .state('menu.trends', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/trends.html',
+        controller: 'trendsCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+  .state('menu.settings', {
+    url: '/page4',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('home', {
+    url: '/page5',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+
+});
